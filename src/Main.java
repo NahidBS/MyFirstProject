c427182290f0bf584db180306411d6a5e6e03788
Nahid Fahim <fahimbinimam@gmail.com>
2025-07-09 17:47:55 +0600

    added java main

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hi! I am Nahid.");
+        System.out.println("I like Pizza!");
+
+        //data types
+        //primitive - int
+        int age = 23;
+        int year = 2025;
+        int quantity = 5;
+        System.out.println("My age is "+age+" in year "+year);
+
+        //primitive - double
+        double price = 20.99;
+        double gpa = 3.8;
+        double temperature = - 12.5;
+        System.out.println("Pizza Price is "+price);
+        System.out.println("Outdoor temperature is "+temperature);
+
+        //primitive char
+        char grade = 'A';
+        char symbol = '!';
+        char curency = '$';
+        System.out.println("My grade is "+grade);
+
+
+    }
+}
