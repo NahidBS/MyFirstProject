c6857aef3ce776413638715f61d305deae8d6d95
NahidBS <fahim.imam@brainstation-23.com>
2025-07-16 12:03:53 +0600

    forEach Loop example

---
+++ src/ForEach.java
@@ -0,0 +1,13 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford"};
+        for(String i: cars){
+            System.out.println(i);
+        }
+
+        String[] name = {"Nahid", "Fahim", "MD. Fahim Bin Imam Nahid"};
+        for(String i: name){
+            System.out.println(i);
+        }
+    }
+}
